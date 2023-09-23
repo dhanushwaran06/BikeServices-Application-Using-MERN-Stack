ea4f1f2ae27838763818513802ff1702fe922601
dhanushwaran06 <ajdhanush.jvl@gmail.com>
2023-09-23 20:37:52 +0530

    commit

--- Client/src/pages/ServicesCard.jsx
+++ Client/src/pages/ServicesCard.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import PropTypes from 'prop-types';
-// import axios from 'axios';   
+import axios from 'axios';   
 
 const ServiceCard = ({ service }) => {
   const [showBookingForm, setShowBookingForm] = useState(false);
@@ -10,18 +10,18 @@ const ServiceCard = ({ service }) => {
     setShowBookingForm(!showBookingForm);
   };
 
-//   const submitService = () => {
-//     axios.post('http://localhost:5000/book-service', {
-//         body: {
-//             ...serviceDetails,
-//             status: 'servicing',
-//             serviceType: service.serviceType,
-//             shop: service.shop,
+  const submitService = () => {
+    axios.post('http://localhost:5000/book-service', {
+        body: {
+            ...serviceDetails,
+            status: 'servicing',
+            serviceType: service.serviceType,
+            shop: service.shop,
 
-//         }
-//     }).then((response) => {console.log(response);})
-//     .catch((error) => {console.log(error);})
-//   }
+        }
+    }).then((response) => {console.log(response);})
+    .catch((error) => {console.log(error);})
+  }
 
   const handleEmailChange = (e) => {
     setServiceDetails({
@@ -136,7 +136,7 @@ const ServiceCard = ({ service }) => {
                 <button
                   type="submit"
                   className="text-white bg-green-500 py-2 px-4 rounded hover:bg-green-600"
-                //   onClick={submitService}
+                  onClick={submitService}
                 >
                   Book
                 </button>
